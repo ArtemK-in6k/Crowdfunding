e004ba2e964ba6b1d8400fdf3eee12b81a6ed159
employee <ArtemK-int6k>
2016-04-12 18:27:52 +0300

    add js controller for admin

--- src/main/webapp/resources/angular/controllers.js
+++ src/main/webapp/resources/angular/controllers.js
@@ -11,3 +11,21 @@ crowdfundingApp.controller('ProjectList', ['$scope', '$http', function ($scope, 
     ;
     $scope.orderProp = '';
 }]);
+
+crowdfundingApp.controller('ProjectListForAdmin', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/projects').success(function (data) {
+        $scope.projects = data;
+    }).error(function (datat) {
+        console.log(datat);
+    });
+
+    $scope.updateStatus = function (status, id) {
+        var project = {
+            "id" : id,
+            "status" : status
+        }
+        $http.post("/admin/savestatus/", project).success(function(data, status) {
+            $scope.projects = data;
+        })
+    }
+}]);
