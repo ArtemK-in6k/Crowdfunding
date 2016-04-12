bad26a3820375cd2b32804b4d4a4769eb98ebcd1
volodymyrO <volodymyro@interlink-ua.com>
2016-04-12 15:12:41 +0300

    Add method for getting all categories with base fields

--- src/main/java/com/crowd/service/CategoryService.java
+++ src/main/java/com/crowd/service/CategoryService.java
@@ -1,11 +1,15 @@
 package com.crowd.service;
 
+import com.crowd.bean.category.SimpleCategoryBean;
 import com.crowd.dao.CategoyDAO;
 import com.crowd.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -19,5 +23,13 @@ public class CategoryService {
 
     public Category findById(int id) {
         return categoyDAO.findById(id);
+    }
+
+    public ResponseEntity<List<SimpleCategoryBean>> getAllCategories(){
+        List<SimpleCategoryBean> categories = categoyDAO.selectAll()
+                .stream().map(SimpleCategoryBean::new)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<List<SimpleCategoryBean>>(categories, HttpStatus.OK);
     }
 }
