4fef3ac26774ac8aa7c5d814dc4d4fe4a3801dc0
employee <ArtemK-int6k>
2016-04-13 11:34:28 +0300

    add method delete project

--- src/main/webapp/resources/angular/adminProjectsController.js
+++ src/main/webapp/resources/angular/adminProjectsController.js
@@ -22,4 +22,37 @@ crowdfundingApp.controller('ProjectListForAdmin', ['$scope', '$http','$timeout',
         })
     }
 
+
+    $scope.deleteProject = function (id,name) {
+        var project = {
+            "id": id
+        }
+        $http.post("/admin/deleteProject/", project).success(function (data) {
+            $scope.projects = data;
+            $scope.projectDelete = name;
+            $scope.projectDeleteSuccess = true;
+
+            $timeout(function () {
+                $scope.projectDeleteSuccess = false;
+            },3000);
+        })
+
+    }
 }]);
+
+crowdfundingApp.directive('ngConfirmClick', [
+    function(){
+        return {
+            link: function (scope, element, attr) {
+                var msg ="Would you delete project " + attr.ngConfirmClick +"? \nYou can lost all data for this project." || "Are you sure?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click',function (event) {
+                    if ( window.confirm(msg) ) {
+                        scope.$eval(clickAction)
+                    }
+                });
+            }
+        };
+    }])
+
+
