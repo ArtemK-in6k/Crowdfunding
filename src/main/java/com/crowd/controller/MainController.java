3b91bda73fdf8dfab21427e5b2f3c3cda7d508b3
volodymyrO <volodymyro@interlink-ua.com>
2016-04-13 09:24:27 +0300

    Using root path

--- src/main/java/com/crowd/controller/MainController.java
+++ src/main/java/com/crowd/controller/MainController.java
@@ -5,13 +5,16 @@ import com.crowd.entity.Project;
 import com.crowd.service.CategoryService;
 import com.crowd.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping({"","/"})
@@ -23,19 +26,16 @@ public class MainController {
     ProjectService projectService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String redirect(){
-        return "redirect:/main";
-    }
-
-    @RequestMapping(value = "main", method = RequestMethod.GET)
-    public String main(Model model, HttpSession session) {
+    public String redirect(Model model, HttpSession session, Principal principal){
+        if (Objects.isNull(principal)){
+            return "redirect:/login";
+        }
 
         List<Category> categories = categoryService.selectAll();
         List<Project> projects = projectService.selectAll();
 
         model.addAttribute("projects",projects);
         session.setAttribute("categories",categories);
-
         return "main";
     }
 
