2ec6943f5abab1b4a2f22859ef858c422a7c2d6f
volodymyrO <volodymyro@interlink-ua.com>
2016-04-14 17:06:12 +0300

    Write service method for creating donation

--- src/main/java/com/crowd/service/DonateService.java
+++ src/main/java/com/crowd/service/DonateService.java
@@ -4,13 +4,17 @@ import com.crowd.bean.donate.DonateBean;
 import com.crowd.bean.donate.ProjectDonatesResponse;
 import com.crowd.dao.DonateDAO;
 import com.crowd.dao.ProjectDAO;
+import com.crowd.dao.UserDAO;
 import com.crowd.entity.Donate;
 import com.crowd.entity.Project;
+import com.crowd.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,6 +25,7 @@ public class DonateService {
 
     @Autowired private DonateDAO donateDAO;
     @Autowired private ProjectDAO projectDAO;
+    @Autowired private UserDAO userDAO;
 
     public List<Donate> selectAll() {
         return donateDAO.selectAll();
@@ -39,6 +44,25 @@ public class DonateService {
 
         ProjectDonatesResponse response = new ProjectDonatesResponse(project);
         return new ResponseEntity<ProjectDonatesResponse>(response, HttpStatus.OK);
+    }
+
+    public boolean addDonateForProject(float amount, int projectId, int donatorId){
+        Project project = projectDAO.findById(projectId);
+        User donator = userDAO.findById(donatorId);
+        
+        if (Objects.isNull(project) || Objects.isNull(donator)){
+            throw new IllegalArgumentException("Input parameters can't be null");
+        }
+        
+        Donate donate = new Donate();
+        donate.setDate(new Timestamp(System.currentTimeMillis()));
+        donate.setAmount(amount);
+        donate.setProject(project);
+        donate.setUser(donator);
+
+        donateDAO.insert(donate);
+
+        return true;
     }
 
 }
