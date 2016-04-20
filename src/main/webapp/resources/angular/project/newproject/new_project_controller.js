62a8072ece246aacdb748ed18655706fce4d2c70
employee <ArtemK-int6k>
2016-04-20 18:56:43 +0300

    add controller for editor

---
+++ src/main/webapp/resources/angular/project/newproject/new_project_controller.js
@@ -0,0 +1,5 @@
+var crowdfundingApp = angular.module('crowdfundingApp',['textAngular']);
+
+crowdfundingApp.controller('AppCtrl', function($scope) {
+    $scope.htmlcontent = "Default message";
+});
