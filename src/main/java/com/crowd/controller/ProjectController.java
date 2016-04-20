1417f0dcec20fb8ab7ba9e359e0c262831f8da5d
employee <ArtemK-int6k>
2016-04-20 19:16:24 +0300

    add response json project by id

--- src/main/java/com/crowd/controller/ProjectController.java
+++ src/main/java/com/crowd/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package com.crowd.controller;
 
 
+import com.crowd.bean.ProjectResponse;
+import com.crowd.bean.donate.UserDonatesBean;
 import com.crowd.bean.user.UserBean;
 import com.crowd.entity.Category;
 import com.crowd.entity.Donate;
@@ -9,6 +11,8 @@ import com.crowd.service.CategoryService;
 import com.crowd.service.DonateService;
 import com.crowd.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -50,14 +54,21 @@ public class ProjectController {
 
     @RequestMapping(method = RequestMethod.POST)
     public String createProject(@ModelAttribute("userBean") UserBean user,@RequestParam String projectName, @RequestParam double needAmount,
-                                @RequestParam String image, @RequestParam int category, @RequestParam String aboutProject) throws IOException {
+                                @RequestParam String image, @RequestParam String aboutProject) throws IOException {
 
-       int projectId = projectService.createProject(user,projectName,needAmount,image,category,aboutProject);
+       int projectId = projectService.createProject(user,projectName,needAmount,image,aboutProject);
         return "redirect:/projects/"+projectId;
     }
 
     @RequestMapping(value = "/new",method = RequestMethod.GET)
     public String createProjectPage() {
         return "newproject";
+    }
+
+    @RequestMapping(value = "/project/{id}", method = RequestMethod.GET)
+    public ResponseEntity<ProjectResponse> getProjectById(@PathVariable int id) {
+        ProjectResponse projects = new ProjectResponse(projectService.findById(id));
+        return new ResponseEntity<ProjectResponse>(projects, HttpStatus.OK);
+
     }
 }
