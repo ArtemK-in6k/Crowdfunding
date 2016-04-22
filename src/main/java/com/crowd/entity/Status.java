59a3084d65e5b09753bc145efe74edfb03d85ed9
employee <ArtemK-int6k>
2016-04-22 17:23:16 +0300

    add constructor and getter

--- src/main/java/com/crowd/entity/Status.java
+++ src/main/java/com/crowd/entity/Status.java
@@ -1,5 +1,19 @@
 package com.crowd.entity;
 
 public enum Status {
-        NOT_STARTED, IN_PROGRESS, FOUNDED, COMPLETED
+        NOT_STARTED("Not started"),
+        IN_PROGRESS("In progress"),
+        FOUNDED("Founded"),
+        COMPLETED("Completed");
+
+        private String status;
+
+        Status(String status){
+                this.status = status;
+        }
+
+        public String getStatus() {
+                return this.status;
+        }
+
 }
