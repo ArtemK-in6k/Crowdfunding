a34974fd03bae14f0d2b9c8b5985d338828d945a
volodymyrO <volodymyro@interlink-ua.com>
2016-04-25 16:26:27 +0300

    Add controller with method for returning templates

---
+++ src/main/java/com/crowd/controller/TemplateController.java
@@ -0,0 +1,17 @@
+package com.crowd.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/templates")
+public class TemplateController {
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String template(@PathVariable String name){
+        return name;
+    }
+}
