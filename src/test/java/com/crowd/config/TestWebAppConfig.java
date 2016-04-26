529af036776ce4df99329c0564bafd69396c766a
volodymyrO <volodymyro@interlink-ua.com>
2016-04-26 14:09:21 +0300

    Add test web app config

---
+++ src/test/java/com/crowd/config/TestWebAppConfig.java
@@ -0,0 +1,13 @@
+package com.crowd.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.crowd")
+public class TestWebAppConfig extends WebMvcConfigurerAdapter {
+
+}
