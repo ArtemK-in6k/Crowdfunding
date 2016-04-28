f31dbfad072155998395d3856671c107085add15
volodymyrO <volodymyro@interlink-ua.com>
2016-04-28 17:01:53 +0300

    Add tests for donate approve method

---
+++ src/main/java/com/crowd/controller/api/DonateApiController.java
@@ -0,0 +1,23 @@
+package com.crowd.controller.api;
+
+import com.crowd.service.DonateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/api/donates")
+@RestController
+public class DonateApiController {
+
+    @Autowired
+    private DonateService donateService;
+
+    @RequestMapping(value = "{donateId}/approve", method = RequestMethod.POST)
+    public ResponseEntity<Boolean> approveDonate(@PathVariable Integer donateId){
+        return donateService.approveDonate(donateId);
+    }
+
+}
